0a18ebae0aede262668319ddb98a4d978b3424e3
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2024-02-16 05:10:06 +0600

    updated

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ app.get("/", (req, res) => {
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-// const uri = "mongodb://0.0.0.0:27017/";
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1ranzbu.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb://0.0.0.0:27017/";
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1ranzbu.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-// const client = new MongoClient(uri);
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+const client = new MongoClient(uri);
 
 async function run() {
   try {
@@ -38,6 +38,12 @@ async function run() {
     // all get functions are here
     app.get("/allDataofCSE13", async (req, res) => {
       const cursor = cse13batch.find({});
+      const students = await cursor.toArray();
+      res.send(students);
+    });
+
+    app.get("/allStudentsMailID", async (req, res) => {
+      const cursor = cse13batch.find({}, { projection: { email: 1, id: 1 } });
       const students = await cursor.toArray();
       res.send(students);
     });
